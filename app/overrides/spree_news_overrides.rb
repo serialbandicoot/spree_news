a7ccda804080750660153a77cfdaa8b2a0a88941
Sam <samtreweek@gmail.com>
2012-03-27 09:37:55 +0100

    Changed the location of the ticker

--- app/overrides/spree_news_overrides.rb
+++ app/overrides/spree_news_overrides.rb
@@ -4,8 +4,15 @@ Deface::Override.new(:virtual_path => "spree/layouts/admin",
                      :text => "<%= tab(:posts) %>",
                      :disabled => false)
 
-Deface::Override.new(:virtual_path => "spree/home/index",
-                     :name => "index_news_partial",
-                     :insert_after => "[data-hook='homepage_products']",
+#Deface::Override.new(:virtual_path => "spree/home/index",
+#                     :name => "index_news_partial",
+#                     :insert_after => "[data-hook='homepage_products']",
+#                     #:insert_after => "[data-hook='inside_head']",
+#                     :partial => "shared/news",
+#                     :disabled => false)
+
+Deface::Override.new(:virtual_path => 'spree/shared/_store_menu',
+                     :name => 'index_news_partial',
+                     :insert_before => 'li#home-link',
                      :partial => "shared/news",
                      :disabled => false)
